776e2fef77b72683733039932bafa07267a4d80b
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-10-14 17:55:41 +0600

    add author partial update endpoint

--- src/main/java/com/example/controllers/AuthorController.java
+++ src/main/java/com/example/controllers/AuthorController.java
@@ -67,4 +67,21 @@ public class AuthorController {
                 HttpStatus.OK
         );
     }
+
+    @PatchMapping(path="/authors/{id}")
+    public ResponseEntity<AuthorDto> partialUpdate(
+            @PathVariable("id") Long id,
+            @RequestBody AuthorDto authorDto
+    ){
+        if(!authorService.isExists(id)){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        AuthorEntity authorEntity = authorMapper.mapFrom(authorDto);
+        AuthorEntity updatedAuthor = authorService.partialUpdate(id, authorEntity);
+        return new ResponseEntity<>(
+                authorMapper.mapTo(updatedAuthor),
+                HttpStatus.OK
+        );
+    }
 }
--- src/main/java/com/example/services/AuthorService.java
+++ src/main/java/com/example/services/AuthorService.java
@@ -13,4 +13,6 @@ public interface AuthorService {
     Optional<AuthorEntity> findOne(Long id);
 
     boolean isExists(Long id);
+
+    AuthorEntity partialUpdate(Long id, AuthorEntity authorEntity);
 }
