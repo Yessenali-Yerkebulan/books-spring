f860450f4e508bf73cde4e69a2bd3520fb35c787
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-10-14 19:18:34 +0600

    match nested objects

--- src/main/java/com/example/config/MapperConfig.java
+++ src/main/java/com/example/config/MapperConfig.java
@@ -2,6 +2,8 @@ package com.example.config;
 
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +12,8 @@ public class MapperConfig {
 
     @Bean
     public ModelMapper modelMapper(){
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+        return modelMapper;
     }
 }
